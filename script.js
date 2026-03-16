c5d8736bae89b12deebcc9e9bf5c0b56c2f7b815
wesleyogibbons-ctrl <wesley.o.gibbons@gmail.com>
2026-03-16 19:18:28 -0400

    Increase vertical buffer between cars in the same lane

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const TEAM_CONFIG = {
 
 // Middle-Out Lane Order: Center, Inner-Right, Inner-Left, Outer-Right, Outer-Left
 const LANE_OFFSETS = ["50%", "90%", "10%", "77%", "23%", "38%", "62%"];
-const VERTICAL_BUFFER = 7; // Pixels needed between cars in the same lane
+const VERTICAL_BUFFER = 20; // Pixels needed between cars in the same lane
 
 async function syncData() {
     try {
