b9b833e9947586de3b2679b928927fbf9b955fe7
wesleyogibbons-ctrl <wesley.o.gibbons@gmail.com>
2026-03-16 22:17:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const TEAM_CONFIG = {
 };
 
 // Middle-Out Lane Order: Center, Inner-Right, Inner-Left, Outer-Right, Outer-Left
-const LANE_OFFSETS = ["50%", "90%", "10%", "80%", "20%", "60%", "40%"];
+const LANE_OFFSETS = ["50%", "90%", "10%", "77%", "23%", "38%", "62%"];
 const VERTICAL_BUFFER = 80; // Pixels needed between cars in the same lane
 
 async function syncData() {
